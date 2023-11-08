5f092a40fd0fa8e566ac70ae3b608c6261a9a263
jonas <@DISTRIB_ID=Pop.com>
2023-11-08 20:55:49 +0100

    Disable minicom in upload script

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ set -e
 arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328 main
 arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328 main
 
-minicom -D /dev/ttyUSB0
+# minicom -D /dev/ttyUSB0
